afd817f3b29c6439b8affe8540df6fb34e8f0b4e
nikzdevz <nikzdreamer2001@gmail.com>
2024-01-11 02:32:33 +0530

    Finalized

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+
 import mysql.connector
 from flask import Flask, render_template, session, request, redirect, url_for
 
-from flask_session import Session
 
 app = Flask(__name__)
+app.secret_key = 'jdbjiobd'
 config = {
     'user': 'nikz',
     'password': 'Devznikz#09',
@@ -76,9 +77,6 @@ def contactSubmit():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if session.get('loggedInEmail') is not None:
-        print(session['loggedInEmail'])
-        loggedinEmail = session['loggedInEmail']
     return render_template('index.html')
 
 
@@ -96,20 +94,18 @@ def dashboard():
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
-    session.pop('loggedInEmail')
-    return redirect(url_for('/'))
+    session.pop('loggedInEmail', None)
+    return redirect(url_for('index'))
 
 
 @app.route('/loginRegister', methods=['GET'])
 def loginRegister():
     if session.get('loggedInEmail') is not None:
-        print(session['loggedInEmail'])
         return redirect(url_for('dashboard'))
     return render_template('loginRegister.html')
 
 
 if __name__ == "__main__":
-    app.config["SESSION_PERMANENT"] = False
-    app.config["SESSION_TYPE"] = "filesystem"
-    Session(app)
+    # app.config["SESSION_PERMANENT"] = False
+    # app.config["SESSION_TYPE"] = "filesystem"
     app.run()
