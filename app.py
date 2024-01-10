aa95d159886efa3d9f4f2cd3bfd5d972682f2b73
nikzdevz <nikzdreamer2001@gmail.com>
2024-01-10 19:48:08 +0530

    Initial Landing page

--- app.py
+++ app.py
@@ -1,10 +1,70 @@
-import mysql
+
 import mysql.connector
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, redirect, url_for
 
 from flask_session import Session
 
 app = Flask(__name__)
+
+config = {
+    'user': 'root',
+    'password': '',
+    'host': 'localhost',
+    'port': '3306',
+    'database': 'xenonstackdb'
+}
+connection = mysql.connector.connect(**config)
+cursor = connection.cursor()
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['email']
+    password = request.form['password']
+    # Check if the user exists and if the password matches
+    query = "SELECT * FROM userbase WHERE email = %s AND password = %s"
+    cursor.execute(query, (username, password))
+    user = cursor.fetchone()
+    if user:
+        return redirect(url_for('dashboard'))
+    else:
+        return "Invalid email or password"
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    email = request.form['email']
+    password = request.form['password']
+    confirmPass = request.form['confirmpass']
+    if password != confirmPass:
+        return "Password not matched"
+    # Check if the username already exists
+    query = "SELECT * FROM userbase WHERE email = %s"
+    cursor.execute(query, (email,))
+    existing_user = cursor.fetchone()
+
+    if existing_user:
+        return "Username already exists"
+    else:
+        # Insert the new user into the database
+        insert_query = "INSERT INTO userbase (email,password) VALUES (%s, %s)"
+        cursor.execute(insert_query, (email, password))
+        connection.commit()
+        return redirect(url_for('dashboard'))
+
+
+@app.route('/contactSubmit', methods=['POST'])
+def contactSubmit():
+    fname = request.form['fname']
+    lname = request.form['lname']
+    email = request.form['contactEmail']
+    phone = request.form['contactPhone']
+    message = request.form['comment']
+    # Check if the username already exists
+    insert_query = "INSERT INTO contactus (Fname, Lname,Email, Phone, Message) VALUES (%s, %s, %s,%s,%s)"
+    cursor.execute(insert_query, (fname, lname, email, phone, message))
+    connection.commit()
+    return redirect(url_for('index'))
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,8 +80,14 @@ def index():
 def contact():
     return render_template('contactus.html')
 
-@app.route('/login', methods=['GET'])
-def login():
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/loginRegister', methods=['GET'])
+def loginRegister():
     return render_template('loginRegister.html')
 
 
